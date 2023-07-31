7e31ebe48d45a99f86e8b0e2e44cef9f42ba8402
Etana Kopin <claws.33@gmail.com>
2023-07-31 11:19:10 -0600

    delete boilerplate components

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,49 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default { //This object is the App that is being imported into the main.ts file createApp(*App*)
+    data () {
+      return {
+        message: 'It works',
+        counterTitle: 'Standard Counter',
+        count: 0,
+        incrementAmount: 4,
+        listOfNumbers: [1,2,3,4]
+      }
+    },
+    methods: {
+      incrementCount(number, event) {
+        this.count += this.incrementAmount
+      }
+    },
+    computed: {
+      displayTitle() {
+        if (this.count > 20){
+          return 'Long Counter'
+        } else {
+          return 'Standard Counter'
+        }
+      },
+      optimizedIncrementAmount(){
+        return this.displayTitle.length * this.incrementAmount
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p v-if="message.length % 2 === 0">Even: {{message.toUpperCase()}}</p>
+  <p v-else>Odd: {{message}}</p>
+  <ul>
+    <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{ number }}</li>
+  </ul>
+  <hr>
+  
+  <h1>{{displayTitle}}</h1>
+  <h3>optimized amount: {{optimizedIncrementAmount}}</h3> 
+  <p>This optimized amount is dependent on 2 things, example of tool chaining. </p>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCount(10, $event)">Increment Count</button>
+  <div>
+    <label for="incrementCount">Increment by:</label>
+    <input type="number" v-model="incrementAmount" />
+  </div>
 </template>
-
-<docs>
-
-  Documentation? Don't know where this shows up though
-</docs>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
